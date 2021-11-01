dc638b4d052ee0bf0c4d31cc1af823bbc1f62fa1
fukymaka <fukymaka@gmail.com>
2021-11-02 01:23:16 +0300

    temp

---
+++ src/OzonEdu.MerchApi.Domain/AggregationModels/MerchItemAggregate/Interfaces/IMerchItemRepository.cs
@@ -0,0 +1,9 @@
+using OzonEdu.MerchApi.Domain.Contracts;
+
+namespace OzonEdu.MerchApi.Domain.AggregationModels.MerchItemAggregate.Interfaces
+{
+    public interface IMerchItemRepository : IRepository<MerchItem>
+    {
+        
+    }
+}
--- src/OzonEdu.MerchApi.Domain/AggregationModels/MerchItemAggregate/ValueObjects/MerchType.cs
+++ src/OzonEdu.MerchApi.Domain/AggregationModels/MerchItemAggregate/ValueObjects/MerchType.cs
@@ -4,6 +4,7 @@ namespace OzonEdu.MerchApi.Domain.AggregationModels.MerchItemAggregate
 {
     public class MerchType : Enumeration
     {
+        //MerchType это типа ВелкомПак, который хранит несколько Sku
         public static MerchType WelcomePack = new(1, nameof(WelcomePack), new Sku(228));
 
         public Sku Sku;
