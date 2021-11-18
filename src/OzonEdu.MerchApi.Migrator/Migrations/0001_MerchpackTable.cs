eba7938526e119390fd7cee21ee6389b031b5253
fukymaka <fukymaka@gmail.com>
2021-11-18 21:25:17 +0300

    add migrator project

---
+++ src/OzonEdu.MerchApi.Migrator/Migrations/0001_MerchpackTable.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace OzonEdu.MerchApi.Migrator.Migrations
+{
+    [Migration(1)]
+    public class MerchpackTable : Migration
+    {
+        public override void Up()
+        {
+            
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/OzonEdu.MerchApi.Migrator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OzonEdu.MerchApi.Migrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
